255d169c4d78fd36a9db25d2249e8dd9662b9144
Eslam abselnasser <96479431+imsemoo@users.noreply.github.com>
2025-02-05 12:13:26 +0200

    Signed-off-by: Eslam abselnasser <96479431+imsemoo@users.noreply.github.com>

--- js/script.js
+++ js/script.js
@@ -1,131 +1,61 @@
 $(document).ready(function () {
-  // عند التمرير
   $(window).on("scroll", function () {
-    if ($(window).scrollTop() > 50) {
-      $(".navbar").addClass("fixed-nav");
-    } else {
-      $(".navbar").removeClass("fixed-nav");
-    }
+    $(".navbar").toggleClass("fixed-nav", $(window).scrollTop() > 50);
   });
 
-  $(".studies-carousel").owlCarousel({
-    loop: true, // Continuous loop
-    margin: 0, // Spacing between items
-    nav: true, // Show navigation arrows
-    rtl: true, // Right-to-left direction
-    dots: false, // Hide dots
-    navText: [
-      "<i class='fa-solid fa-chevron-right'></i>",
-      "<i class='fa-solid fa-chevron-left'></i>",
-    ],
-    responsive: {
-      0: {
-        stagePadding: 50,
-        items: 1,
-      }, // On small screens: 1 item
-      576: { items: 2 }, // On medium screens: 2 items
-      992: { items: 3 }, // On large screens: 3 items
-    },
-  });
-
-  $(".research-carousel").owlCarousel({
-    rtl: true, // Enable RTL for Arabic layout
-    loop: true, // Infinite looping of slides
-    margin: 20, // Space between items
-    nav: true, // Show next/prev arrows
-    dots: false, // Hide pagination dots (can be true if desired)
-    navText: [
-      "<i class='fa-solid fa-chevron-right'></i>",
-      "<i class='fa-solid fa-chevron-left'></i>",
-    ],
-    responsive: {
-      0: {
-        stagePadding: 50,
-
-        items: 1,
-      },
-      768: {
-        items: 2,
-      },
-      992: {
-        items: 3,
-      },
-    },
-  });
-  $(".reports-carousel").owlCarousel({
-    rtl: true, // Enable RTL for Arabic
-    loop: true, // Infinite loop
-    margin: 20, // Space between slides
-    nav: true, // Show prev/next arrows
-    dots: false, // Hide dots (optional)
-    navText: [
-      '<i class="fa-solid fa-chevron-right"></i>',
-      '<i class="fa-solid fa-chevron-left"></i>',
-    ],
-    responsive: {
-      0: {
-        stagePadding: 50,
-
-        items: 1,
-      },
-      992: {
-        items: 1, // We show one "row" with 3 cards in that row (2 small + 1 big)
-      },
-    },
-  });
-  $(".courses-carousel").owlCarousel({
-    rtl: true, // Use RTL for Arabic layout
-    loop: true, // Infinite loop
-    margin: 20, // Spacing between cards
-    nav: true, // Display next/prev arrows
-    dots: false, // Hide pagination dots (optional)
-    navText: [
-      '<i class="fa-solid fa-chevron-right"></i>',
-      '<i class="fa-solid fa-chevron-left"></i>',
-    ],
-    responsive: {
-      0: {
-        stagePadding: 50,
-
-        items: 1,
-      },
-      576: {
-        items: 2,
-      },
-      768: {
-        items: 3,
-      },
-      992: {
-        items: 4,
-      },
-    },
-  });
-  $(".team-slider").owlCarousel({
-    // Enable infinite loop for continuous scrolling
-    loop: true,
-    // Set margin between slider items in pixels
-    margin: 10,
-    // Enable navigation arrows for slider control
-    navText: [
-      '<i class="fa-solid fa-chevron-right"></i>',
-      '<i class="fa-solid fa-chevron-left"></i>',
-    ],
-    nav: true,
-    // Activate right-to-left mode for proper Arabic display
+  var commonOptions = {
     rtl: true,
-    // Responsive settings for different viewport widths
-    responsive: {
-      0: {
-        stagePadding: 50,
+    loop: true,
+    nav: true,
+    dots: false,
+    navText: [
+      '<i class="fa-solid fa-chevron-right"></i>',
+      '<i class="fa-solid fa-chevron-left"></i>',
+    ]
+  };
 
-        items: 1, // Display 1 item on small screens
-      },
-      600: {
-        items: 4, // Display 2 items on medium screens
-      },
-      1000: {
-        items: 6, // Display 3 items on large screens
-      },
-    },
-  });
+  $(".studies-carousel").owlCarousel($.extend({}, commonOptions, {
+    margin: 0,
+    responsive: {
+      0: { stagePadding: 50, items: 1 },
+      576: { items: 2 },
+      992: { items: 3 }
+    }
+  }));
+
+  $(".research-carousel").owlCarousel($.extend({}, commonOptions, {
+    margin: 20,
+    responsive: {
+      0: { stagePadding: 50, items: 1 },
+      768: { items: 2 },
+      992: { items: 3 }
+    }
+  }));
+
+  $(".reports-carousel").owlCarousel($.extend({}, commonOptions, {
+    margin: 20,
+    responsive: {
+      0: { stagePadding: 50, items: 1 },
+      992: { items: 1 }
+    }
+  }));
+
+  $(".courses-carousel").owlCarousel($.extend({}, commonOptions, {
+    margin: 20,
+    responsive: {
+      0: { stagePadding: 50, items: 1 },
+      576: { items: 2 },
+      768: { items: 3 },
+      992: { items: 4 }
+    }
+  }));
+
+  $(".team-slider").owlCarousel($.extend({}, commonOptions, {
+    margin: 10,
+    responsive: {
+      0: { stagePadding: 50, items: 1 },
+      600: { items: 4 },
+      1000: { items: 6 }
+    }
+  }));
 });
